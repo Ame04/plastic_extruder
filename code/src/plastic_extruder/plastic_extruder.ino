9c0f2a61446b730d57b51267fb2b20d3b82e5f06
arme04 <rugby.man0406@gmail.com>
2024-02-17 18:30:40 +0100

    Changed project structur and added include file

--- code/src/plastic_extruder/plastic_extruder.ino
+++ code/src/plastic_extruder/plastic_extruder.ino
@@ -1,27 +1,33 @@
 /**
- * @file main.ino
+ * @file plastic_extruder.ino
  * @author CAMUS Armand
- * @date 04/02/2024
+ * @date 17/02/2024
+ * @brief Includes, defines and function signatures
 */
 
 /*******************************************************************************
  * Includes
 ********************************************************************************/
-
+#include <Wire.h>
+#include <ContinuousStepper.h>
+#include <ContinuousStepper/Tickers/TimerOne.hpp>
+#include <LiquidCrystal_I2C.h>
 /*******************************************************************************
  * Defines
 ********************************************************************************/
+/* Stepper defines */
+#define STEPPER_PIN_1 9
+#define STEPPER_PIN_2 10
+#define STEPPER_PIN_3 11
+#define STEPPER_PIN_4 12
+
+/* LCD defines */
+#define LCD_ADDR  0x27
+#define LCD_ROW 2
+#define LCD_COLLUMN 16
 
 /*******************************************************************************
  * Global varibales
 ********************************************************************************/
-
-
-
-void setup(void) {
-
-}
-
-void loop(void){
-
-}
+ContinuousStepper<FourWireStepper, TimerOneTicker> winding_stepper;
+LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLLUMN, LCD_ROW);
---
+++ code/src/plastic_extruder/z_main.ino
@@ -0,0 +1,52 @@
+/**
+ * @file z_main.ino
+ * @author CAMUS Armand
+ * @date 04/02/2024
+ * @brief Main of the project
+*/
+
+void setup(void) {
+    winding_stepper.begin(STEPPER_PIN_1, STEPPER_PIN_2, STEPPER_PIN_3, STEPPER_PIN_4);
+    winding_stepper.spin(50);
+
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("LCD initialysed");
+    lcd.setCursor(0,1);
+    lcd.print("Speed = 50");
+
+    Serial.begin(9600);
+    Serial.println(F("System initialised, speed = 40"));
+}
+
+void loop(void){
+    static bool speed = true;
+    static unsigned long previous_time = millis();
+    unsigned long time = millis();
+
+    if (time - previous_time > 5000){
+        Serial.print(F("Time = "));
+        Serial.print(time);
+        lcd.setCursor(0, 0);
+        lcd.print("Time = ");
+        lcd.setCursor(7, 0);
+        lcd.print(time);
+        if (speed){
+            speed = false;
+            winding_stepper.spin(20);
+            Serial.println(F("Speed changed to = 20"));
+            lcd.setCursor(0,1);
+            lcd.print("Speed = 20");
+
+        }
+        else {
+            speed = true;
+            winding_stepper.spin(50);
+            Serial.println(F(" Speed changed to = 50"));
+            lcd.setCursor(0,1);
+            lcd.print("Speed = 50");
+        }
+        previous_time = time;
+    }
+}
